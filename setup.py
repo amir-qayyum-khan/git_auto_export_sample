576f36403cc4807e3a92c20a2bd80af8e804e256
Amir Qayyum Khan <amir.qayyum.khan@gmail.com>
2018-09-25 20:01:01 +0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 import os
-from setuptools import setup
-import git_auto_export
+from setuptools import setup, find_packages
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='git-auto-export-sample',
-    version=git_auto_export.__version__,
-    packages=["git_auto_export"],
+    version='0.1',
+    packages=find_packages(),
     include_package_data=True,
     license='GNU AFFERO GENERAL PUBLIC LICENSE',
     description='A task that auto save course OLX to git when author publish it',
     url='https://github.com/amir-qayyum-khan/git_auto_export_sample',
     install_requires=[
-        'setuptools',
         'edx-opaque-keys',
         'celery',
     ],
